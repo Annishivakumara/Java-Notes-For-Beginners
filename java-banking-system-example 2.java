89161cdc871e691842458bdc12cf53855cd68572
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-11 21:55:26 +0530

    Update java-banking-system-example 2.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

--- java-banking-system-example 2.java
+++ java-banking-system-example 2.java
@@ -1,19 +1,19 @@
 public class BankingSystem {
     public static void main(String[] args) {
-        // Define variables for bank account
+   
         String accountHolderName = "Alice";
         int accountNumber = 123456789;
         double balance = 1000.0;
         boolean isActive = true;
 
-        // Print account information
+        
         System.out.println("Account Holder Name: " + accountHolderName);
         System.out.println("Account Number: " + accountNumber);
         System.out.println("Current Balance: $" + balance);
-        //  Using ternanry Opertor
+     
         System.out.println("Account Status: " + (isActive ? "Active" : "Inactive"));
 
-        // Perform some banking operations
+ 
         double depositAmount = 500.0;
         balance += depositAmount;
         System.out.println("\nDeposited: $" + depositAmount);
@@ -24,7 +24,6 @@ public class BankingSystem {
         System.out.println("\nWithdrawn: $" + withdrawalAmount);
         System.out.println("Updated Balance: $" + balance);
 
-        // Check if account is overdrawn
         if (balance < 0) {
             System.out.println("\nAlert: Account overdrawn! Current Balance: $" + balance);
         }
