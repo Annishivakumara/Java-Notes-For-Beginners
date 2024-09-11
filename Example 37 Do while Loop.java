f93188048b03550626eeea049d7b5440483651c9
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-11 21:53:22 +0530

    Update and rename Example 37 Do while Loop to Example 37 Do while Loop.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

--- Example 37 Do while Loop.java
+++ Example 37 Do while Loop.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//  Importing Package from java library
+
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
