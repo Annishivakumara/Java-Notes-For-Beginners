ccaf6a5274694ab1bebfabc6651866701e782ac5
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-12 20:40:02 +0530

    Update and rename Example 5 to Example 5.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

--- Example 5.java
+++ Example 5.java
@@ -4,18 +4,16 @@ public class LeapYearCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user to enter a year
         System.out.print("Enter a year: ");
         int year = scanner.nextInt();
 
-        // Check if the year is a leap year
+        
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println("The year is a leap year.");
         } else {
             System.out.println("The year is not a leap year.");
         }
 
-        // Close the scanner
-        scanner.close();
+    
     }
 }
