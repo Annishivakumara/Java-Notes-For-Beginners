be26a51236ae08545aa5df5503cd01788897a0a5
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-14 14:29:44 +0530

    Update and rename Exception Handling to Exception Handling.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

---
+++ Object Oriented Programing/Exception/Exception Handling.java
@@ -0,0 +1,4 @@
+Comming Soon
+Exception Handling in Java is one of the effective means to handle runtime errors so that the regular flow of the 
+application can be preserved. 
+Java Exception Handling is a mechanism to handle runtime errors such as ClassNotFoundException, IOException, SQLException,
