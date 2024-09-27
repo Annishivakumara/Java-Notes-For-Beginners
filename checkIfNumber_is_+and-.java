bb62e8b01caf6573d079ff469e66f37f59b54757
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-27 21:01:21 +0530

    Update and rename Check if a Number is (+)(-) Example  5 to checkIfNumber_is_+and-.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

--- checkIfNumber_is_+and-.java
+++ checkIfNumber_is_+and-.java
@@ -4,11 +4,11 @@ public class NumberCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //  enter a number
+        
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        // Check if the number is positive, negative, or zero
+      
         if (number > 0) {
             System.out.println("The number is positive.");
         }
