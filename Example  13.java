f7bce01ce9495f45700ec72925f6bbb0b6ce8952
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-01 23:05:06 +0530

    Update Example  13.java
    Signed-off-by: Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>

--- Example  13.java
+++ Example  13.java
@@ -4,17 +4,16 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user to enter two numbers
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
 
-        // Prompt user to enter an operator
+
         System.out.print("Enter an operator (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
 
-        // Perform the operation
+ 
         switch (operator) {
             case '+':
                 System.out.println("Result: " + (num1 + num2));
@@ -22,21 +21,20 @@ public class SimpleCalculator {
             case '-':
                 System.out.println("Result: " + (num1 - num2));
                 break;
-            case '*':
+     case '*':
                 System.out.println("Result: " + (num1 * num2));
                 break;
-            case '/':
+ case '/':
                 if (num2 != 0) {
                     System.out.println("Result: " + (num1 / num2));
-                } else {
+ } else {
                     System.out.println("Error: Division by zero.");
                 }
-                break;
+break;
             default:
                 System.out.println("Invalid operator. Please enter one of +, -, *, /.");
         }
 
-        // Close the scanner
-        scanner.close();
+    
     }
 }
