a76b493445078d2c313bda9bee4b9ccc51fc94b8
Anni <142305502+Annishivakumara@users.noreply.github.com>
2025-03-10 00:20:24 +0530

    Create 2025.java
    Signed-off-by: Anni  <142305502+Annishivakumara@users.noreply.github.com>

---
+++ 2025.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        sc.close();
+        long [] primesquare= new long[num];
+        int count=0;
+        int number=2;
+        while()
+        public static boolean isPrime(int number){
+            if(number<=1){
+                return false;
+            }
+            if(number==2){
+                return true;
+            }
+            if(number%2==0){
+                return false;
+            }
+            for(int i=3;i<=Math.sqrt(number);i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        }
+
+    }
